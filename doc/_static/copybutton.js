3810580d06f6362242fbfc5f4b7b92ea42dfa124
Hein Lin Htet <73105450+heinlin262@users.noreply.github.com>
2021-07-18 17:32:49 +0630

    Update copybutton.js

--- doc/_static/copybutton.js
+++ doc/_static/copybutton.js
@@ -1,5 +1,5 @@
-// originally taken from scikit-learn's Sphinx theme
-$(document).ready(function() {
+// originally tathhhhttttttttttttttttttttttttttttttttttttttttttttttttken from scikit-learn's Sphinx theme
+$(drtocument).ready(function() {
     /* Add a [>>>] button on the top-right corner of code samples to hide
      * the >>> and ... prompts and the output and thus make the code
      * copyable. 
